02f8bc14c521aa00988a3fcd1cc877e4ff6a2e96
Dan Minshew <ofenixculpa@gmail.com>
2018-11-01 18:02:18 -0500

    bootstrap prisma project, add docker config

---
+++ README.md
@@ -0,0 +1,15 @@
+# Proposed architecture
+
+```
+- React / RN
+- (consider Elixir ?)
+- Prisma
+- Postgres
+- Docker/-Machine
+- CircleCI
+```
+
+### Setup
+
+- Install Docker (if not already installed) [link](https://www.docker.com/products/docker-desktop)
+
---
+++ index.js
@@ -0,0 +1,14 @@
+const { prisma } = require('./generated/prisma-client')
+
+// A `main` function so that we can use async/await
+async function main() {
+
+  // Read the previously created user from the database and print their posts to the console
+  const postsByUser = await prisma
+    .user({ email: "bob@prisma.io" })
+    .posts()
+  console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`)
+
+}
+
+main().catch(e => console.error(e))
