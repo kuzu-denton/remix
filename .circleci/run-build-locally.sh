6fac423ddd418ac5f2dcf4803d7ecfcf730230f4
Dan Minshew <ofenixculpa@gmail.com>
2018-11-02 14:57:13 -0500

    trigger build

--- .circleci/run-build-locally.sh
+++ .circleci/run-build-locally.sh
@@ -2,7 +2,7 @@
 
 curl --user ${CIRCLE_TOKEN}: \
     --request POST \
-    --form revision=<commit_hash>\
+    --form revision=b74e398ed58647b76166a36ecefed195480d0b59\
     --form config=./config.yml \
     --form notify=false \
         https://circleci.com/api/v1.1/project/github/kuzu-denton/remix/tree/master
