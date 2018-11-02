c708d1b7fc1ce4e21ec0e40328639bdab0b3ef96
Dan Minshew <ofenixculpa@gmail.com>
2018-11-02 12:46:18 -0500

    excluse generated client from linting

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { prisma } = require('./generated/prisma-client')
+import { prisma } from './generated/prisma-client'
 
 // A `main` function so that we can use async/await
 async function main() {
 
   // Read the previously created user from the database and print their posts to the console
   const postsByUser = await prisma
-    .user({ email: "bob@prisma.io" })
+    .user({ email: "alice@prisma.io" })
     .posts()
   console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`)
 
